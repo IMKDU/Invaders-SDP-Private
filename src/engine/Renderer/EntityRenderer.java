aa989980d37577df9ca74d965cabd477eea111fb
rioluvy <s0ftrisus@gmail.com>
2025-11-13 16:15:31 +0900

    feat: Implemented BossPattern_BlackHole

--- src/engine/Renderer/EntityRenderer.java
+++ src/engine/Renderer/EntityRenderer.java
@@ -1,6 +1,7 @@
 package engine.Renderer;
 
 import java.awt.Graphics;
+import java.awt.Color;
 import java.util.Map;
 
 import engine.BackBuffer;
@@ -33,6 +34,18 @@ public final class EntityRenderer {
                     g.drawRect(positionX + i * 2, positionY + j * 2, 1, 1);
                 }
             }
+        }
+    }
+
+    /** Draw circle for pull_attack pattern */
+    public void drawBlackHole(final int cx, final int cy, final int radius, final boolean isBlackHoleActive){
+        if(isBlackHoleActive){
+            int x = cx - radius/2;
+            int y = cy - radius/2;
+
+            Graphics g = backBuffer.getGraphics();
+            g.setColor(new Color (200, 0, 255));
+            g.drawOval(x, y, radius, radius);
         }
     }
 }
