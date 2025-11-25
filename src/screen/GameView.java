89cd3892b9dfea26ec391daca8d99a7a61b4dba6
hums <hum4310@naver.com>
2025-11-26 01:33:45 +0900

    fix: fix finalboss image becasue hitbox is not match image size

--- src/screen/GameView.java
+++ src/screen/GameView.java
@@ -32,7 +32,7 @@ public class GameView {
         drawManager.initDrawing(dto.getWidth(), dto.getHeight());
         //오리진 스킬 애니메이션 스킬횟수 > 0 그리고 p1점수 500이상일때 발동
         // 구현 해야할거 발동후 로직, 아군 ship 무적
-        if (model.getFinalSkillCnt() > 0 && (dto.getScoreP1() >= 500)) {
+        if (model.getFinalSkillCnt() > 0 && (dto.getScoreP1() >= 5000000)) {
             drawManager.getSpecialAnimationRenderer().update(model.getCurrentLevel().getLevel());
             drawManager.getSpecialAnimationRenderer().draw();
             if (drawManager.getSpecialAnimationRenderer().isFinished()){
