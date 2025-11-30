3bb319949d99f27e2d636b2d5459fc770b7f7f50
donguk_kim <kampo5096@hanyang.ac.kr>
2025-11-30 22:00:41 +0900

    fix-SCRUM-47: add player charging skill updated logic

--- src/entity/Ship.java
+++ src/entity/Ship.java
@@ -170,6 +170,11 @@ public class Ship extends Entity implements Collidable {
                 }
             }
         }
+
+		// Update charging skill state
+		if (this.chargingSkill != null) {
+			this.chargingSkill.update();
+		}
     }
     /**
      * Register user skills into skill map.
