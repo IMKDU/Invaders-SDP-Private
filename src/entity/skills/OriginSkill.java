f73728f72e9f36793fbb0ff55d0665f91b3c3f0d
rioluvy <s0ftrisus@gmail.com>
2025-11-30 20:11:56 +0900

    fix-SCRUM-45: remove_test_code

--- src/entity/skills/OriginSkill.java
+++ src/entity/skills/OriginSkill.java
@@ -7,7 +7,7 @@ import entity.pattern.ISkill;
 public class OriginSkill implements ISkill {
 
     private static final int TOTAL_DAMAGE = 80;
-    private static final int DURATION_MS = 10000;
+    private static final int DURATION_MS = 11000;
 
     private static final int BURST_WINDOW_MS = 1000;
     private static final int BURST_HITS = (int)(TOTAL_DAMAGE*0.2);
