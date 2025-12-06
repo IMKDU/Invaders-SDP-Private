da052cae694adfe3dca57d72664b22983fdeaacb
rioluvy <s0ftrisus@gmail.com>
2025-12-06 22:22:33 +0900

    feature-SCRUM-85: CI test testing

--- test/entity/BulletPoolTest.java
+++ test/entity/BulletPoolTest.java
@@ -1,6 +1,7 @@
 package entity;
 
-import engine.bullet.*;
+import entity.bullet.Bullet;
+import entity.bullet.BulletPool;
 import engine.shop.ShopItem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
